9440237b09635b9b10fe9eb15ec010734dbe486d
123neetaPandey <123neetaPandey>
2020-06-22 21:42:29 +0530

    my 2nd js commit

---
+++ assgin1/assign.js
@@ -0,0 +1,38 @@
+var l=2;
+var b=9;
+var p=l*b;
+function printarea(l,b){
+    
+    console.log(p);
+    
+}
+
+var firstName='john'
+var age=28;
+console.log(firstName+''+age);
+
+function Greetings() 
+{ 
+    console.log("Welcome to javascript world!"); 
+}; 
+
+Greetings(); 
+  
+
+(function() { 
+    console.log("Welcome to my world!"); })(); 
+    const materials = [
+        'Hydrogen',
+        'Helium',
+        'Lithium',
+        'Beryllium'
+      ];
+      
+      console.log(materials.map(material => material.length));
+var name='saint'
+age=34
+      var job ,isMarried;
+      job='devlper'
+      isMarried=false;
+      console.log(name+'is a '+age+'year old ' +job+'is he married?'+isMarried);
+      
